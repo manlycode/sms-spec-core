ae27595f4b26355f4bb1e58ff987bb2ce490bd5e
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2014-07-24 19:53:06 -0400

    Initial commit.

---
+++ lib/sms_spec/core.rb
@@ -0,0 +1,9 @@
+require 'sms/spec/core/version'
+require 'sms/spec/core/message_center'
+
+module SmsSpec
+  # SmsSpec::Core
+  # Helps you test your sms messages in applications
+  module Core
+  end
+end
---
+++ lib/sms_spec/core/version.rb
@@ -0,0 +1,7 @@
+module Sms
+  module Spec
+    module Core
+      VERSION = "0.0.1"
+    end
+  end
+end
